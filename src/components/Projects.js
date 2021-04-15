3f7c4734f13caa2c98c0803fb0be33fa3350bc0a
Waqar <abdulwaqar844@gmail.com>
2021-04-15 23:50:53 +0500

    skill set updated

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,7 +16,7 @@ const useStyles = makeStyles({
   root: {
     maxWidth: 500,
     textAlign: 'center',
-    backgroundColor:" rgba(206, 188, 188,15%)",
+    backgroundColor: " rgba(206, 188, 188,15%)",
 
 
   },
@@ -30,94 +30,94 @@ export default function MediaCard() {
 
   return (
     <div>
-        <h1 className='project-title' >Projects</h1>
+      <h1 className='project-title' >Projects</h1>
       <hr />
-    <Grid container className="project-list" spacing={1}>
+      <Grid container className="project-list" spacing={1}>
 
-      <Grid item xs={12} sm={4}>
-        <Card className={classes.root}>
-          <CardActionArea>
-            <CardMedia
-              className={classes.media}
-              image={covid}
-              title="Contemplative Reptile"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                Covid Trackinp Application          </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                Covid Tracker Track Data of All infected Patients form all over the World. Record of Covid Patients Country wise also available          </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions className="url-btn">
-            <a href="https://covid19-trackingapp.surge.sh/">
-              <Button size="medium" color="primary">
+        <Grid item xs={12} sm={4}>
+          <Card className={classes.root}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image={covid}
+                title="Contemplative Reptile"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Covid Trackinp Application          </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Covid Tracker Track Data of All infected Patients form all over the World. Record of Covid Patients Country wise also available          </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions className="url-btn">
+              <a href="https://covid19-trackingapp.surge.sh/">
+                <Button size="medium" color="primary">
+                  URL
+        </Button></a>
+              <a href="https://github.com/abdulwaqar844/Covid19-Tracker">  <Button size="medium" color="primary">
+                GitHub
+              </Button>
+              </a>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card className={classes.root}>
+            <CardActionArea >
+              <CardMedia
+                className={classes.media}
+                image={vlolly}
+                title="Virtual Lolly Application"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Virtual Lolly Application
+          </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                  across all continents except Antarctica
+          </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions className="url-btn">
+              <a href="https://virtual-lolly-app.netlify.app/"><Button size="medium" color="primary">
                 URL
         </Button></a>
-            <a href="https://github.com/abdulwaqar844/Covid19-Tracker">  <Button size="medium" color="primary">
-              GitHub
-              </Button>
-            </a>
-          </CardActions>
-        </Card>
-      </Grid>
-      <Grid item xs={12} sm={4}>
-        <Card className={classes.root}>
-          <CardActionArea >
-            <CardMedia
-              className={classes.media}
-              image={vlolly}
-              title="Virtual Lolly Application"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                Virtual Lolly Application
-          </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                across all continents except Antarctica
-          </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions className="url-btn">
-            <a href="https://virtual-lolly-app.netlify.app/"><Button size="medium" color="primary">
-              URL
+              <a href="https://github.com/abdulwaqar844/Virtual-Lolly-app"> <Button size="medium" color="primary">
+                GitHub
         </Button></a>
-            <a href="https://github.com/abdulwaqar844/Virtual-Lolly-app"> <Button size="medium" color="primary">
-              GitHub
-        </Button></a>
-          </CardActions>
-        </Card>
-      </Grid>
-      <Grid item xs={12} sm={4}>
-        <Card className={classes.root}>
-          <CardActionArea >
-            <CardMedia
-              className={classes.media}
-              image={blog}
-              title="Virtual Lolly Application"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-contentful blog    
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card className={classes.root}>
+            <CardActionArea >
+              <CardMedia
+                className={classes.media}
+                image={blog}
+                title="Virtual Lolly Application"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  contentful blog
  </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                across all continents except Antarctica
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                  across all continents except Antarctica
           </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions className="url-btn">
-            <a href="https://contentfulapi-gatsby.netlify.app/blog/"><Button size="medium" color="primary">
-              URL
+              </CardContent>
+            </CardActionArea>
+            <CardActions className="url-btn">
+              <a href="https://contentfulapi-gatsby.netlify.app/blog/"><Button size="medium" color="primary">
+                URL
         </Button></a>
-            <a href="https://github.com/abdulwaqar844/gatsby-netlify-contentful"> <Button size="medium" color="primary">
-              GitHub
+              <a href="https://github.com/abdulwaqar844/gatsby-netlify-contentful"> <Button size="medium" color="primary">
+                GitHub
         </Button></a>
-          </CardActions>
-        </Card>
+            </CardActions>
+          </Card>
+        </Grid>
       </Grid>
-    </Grid>
-</div>
+    </div>
   );
 }
--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,104 +1,37 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
 import LinearProgress from '@material-ui/core/LinearProgress';
-
 import './style.css'
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    textAlign: 'center',
-  },
-  paper: {
-    backgroundColor:" rgba(206, 188, 188,30%)",
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-}));
-
 export default function SkillSet() {
-  const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+
+    <div >
       <h1 >Skills I Have</h1>
-      <hr />
-      <Grid container className="skill-set" spacing={1}>
-        <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper}>
-            <h2>HTML</h2>
-            <label>90%</label>
-            <LinearProgress variant="determinate" value={90} />
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper}>
-            <h2>CSS</h2>
-            <label>80%</label>
-            <LinearProgress variant="determinate" value={80} />
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper}>
-            <h2>JavaScript</h2>
-            <label>70%</label>
-            <LinearProgress variant="determinate" value={70} />
+      <div className="skill-container">
+        <div className="sub-skill"><h3>HTML</h3>
+          <LinearProgress variant="determinate" value={90} />
+        </div>
+        <div className="sub-skill"><h3>CSS</h3>
+          <LinearProgress variant="determinate" value={90} />
+        </div>
+        <div className="sub-skill"><h3>JavaScript</h3>
+          <LinearProgress variant="determinate" value={90} />
+        </div><div className="sub-skill"><h3>Python</h3>
+          <LinearProgress variant="" value={90} />
+        </div>
+        <div className="sub-skill"><h3>Gatsby</h3>
+          <LinearProgress variant="determinate" value={90} />
+        </div><div className="sub-skill"><h3>AWS</h3>
+          <LinearProgress  variant="determinate" value={90} />
+        </div>
+        <div className="sub-skill"><h3>React</h3>
+          <LinearProgress variant="determinate" value={90} />
+        </div><div className="sub-skill"><h3>NoSQL & SQL</h3>
+          <LinearProgress variant="determinate" value={90} />
+        </div>
 
-          </Paper>
-        </Grid>
-      </Grid>
-      <Grid container className="skill-set" spacing={1}>
-        <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper}>
-            <h2>React</h2>
-            <label>90%</label>
-            <LinearProgress variant="determinate" value={90} />
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper}>
-            <h2>Material UI</h2>
-            <label>100%</label>
-            <LinearProgress variant="determinate" value={100} />
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper}>
-            <h2>Gatsby</h2>
-            <label>80%</label>
-            <LinearProgress variant="determinate" value={80} />
-
-          </Paper>
-        </Grid>
-
-      </Grid>
-      <Grid container className="skill-set" spacing={1}>
-        <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper}>
-            <h2>GraphQL</h2>
-            <label>90%</label>
-            <LinearProgress variant="determinate" value={90} />
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper}>
-            <h2>AWS & Netlify Functions</h2>
-            <label>100%</label>
-            <LinearProgress variant="determinate" value={100} />
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper}>
-            <h2>FireBase & Surge</h2>
-            <label>80%</label>
-            <LinearProgress variant="determinate" value={80} />
-
-          </Paper>
-        </Grid>
-
-      </Grid>
+      </div>
+     
     </div>
   );
 }
